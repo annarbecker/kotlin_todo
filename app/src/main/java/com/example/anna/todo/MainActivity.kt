e2805d2f748faa16588db25a3c2d1510e8fdf71b
Anna Becker <anna.r.becker@gmail.com>
2018-03-10 13:05:54 -0800

    Add click listener to the floating action button

--- app/src/main/java/com/example/anna/todo/MainActivity.kt
+++ app/src/main/java/com/example/anna/todo/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.anna.todo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //define floating action button
+        val floatingButton = this.findViewById<View>(R.id.floatingButton)
+
+        // set a click listener on the button
+        this.floatingButton.setOnClickListener { view ->
+
+        }
     }
 }
