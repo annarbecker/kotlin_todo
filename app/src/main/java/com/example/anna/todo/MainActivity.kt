8b0a92a8a9c81f44c652d5dd00c1e92f6faa5b65
Anna Becker <anna.r.becker@gmail.com>
2018-03-10 14:13:18 -0800

    Add addNewItemDialog method which is triggered when the floatingActionButton is clicked

--- app/src/main/java/com/example/anna/todo/MainActivity.kt
+++ app/src/main/java/com/example/anna/todo/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.anna.todo
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.view.View
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +14,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //define floating action button
-        val floatingButton = this.findViewById<View>(R.id.floatingButton)
+        val floatingActionButton = this.findViewById<View>(R.id.floatingActionButton)
 
         // set a click listener on the button
-        this.floatingButton.setOnClickListener { view ->
-
+        this.floatingActionButton.setOnClickListener { view ->
+            this.addNewItemDialog()
         }
+    }
+
+    /*
+     * This method shows a dialog box where the user can enter a new item to add to their To Do list.
+     */
+    private fun addNewItemDialog() {
+        val alert = AlertDialog.Builder(this)
+        val itemEditText = EditText(this)
+
+        alert.setMessage("Add New Item")
+        alert.setTitle("Enter To Do Item")
+
+        alert.setView(itemEditText)
+
+        alert.setPositiveButton("Submit") {dialog, positiveButton -> }
+        alert.show()
     }
 }
