f938244767c4161a8aee5caac1cb2c4482b2c7ac
Anna Becker <anna.r.becker@gmail.com>
2018-03-17 22:07:48 -0700

    Add test that uses Robolectric

--- app/src/test/java/com/example/anna/todo/MainActivityUnitTest.kt
+++ app/src/test/java/com/example/anna/todo/MainActivityUnitTest.kt
@@ -1,14 +1,33 @@
 package com.example.anna.todo
 
+import java_cup.Main
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricGradleTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricGradleTestRunner::class)
+@Config(constants = BuildConfig::class)
 class MainActivityUnitTest {
+
+
     @Test
-    fun createNewToDoItem() {
+    fun test_MainActivity() {
+        val mainActivity = Robolectric.setupActivity(MainActivity::class.java)
+        val title = mainActivity.actionBar.title.toString()
+
+        assertThat(title, `is`("ToDo2"))
+    }
+
+
+    @Test
+    fun test_createNewToDoItem() {
         val mainActivity = MainActivity()
+
         val itemText = "Walk the dog"
 
         val toDoItem = mainActivity.createNewToDoItem(itemText)
